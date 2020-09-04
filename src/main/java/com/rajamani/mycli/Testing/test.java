28d69b1d1e3b9c8ffc85dafd7c46cde4a00c5caf
Rajamani Sarvesh <rajamanisarvesh23@gmail.com>
2020-09-03 19:41:47 -0700

    Closing up webdriver

--- src/main/java/com/rajamani/mycli/Testing/test.java
+++ src/main/java/com/rajamani/mycli/Testing/test.java
@@ -1,7 +1,6 @@
 package com.rajamani.mycli.Testing;
 
-import org.openqa.selenium.Alert;
-import org.openqa.selenium.By;
+
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.springframework.stereotype.Service;
@@ -17,8 +16,7 @@ public class test {
         System.setProperty("webdriver.chrome.driver", "C:\\Users\\rajam\\Downloads\\chromedriver_win32 (1)\\chromedriver.exe");
         WebDriver driver = new ChromeDriver();
         driver.get(url);
-//        driver.findElement(By.linkText("launch meeting")).click();
-        Thread.sleep(5000);
+        Thread.sleep(2000);
         try {
             Robot robot = new Robot();
             robot.keyPress(KeyEvent.VK_LEFT);
@@ -27,10 +25,10 @@ public class test {
             robot.keyRelease(KeyEvent.VK_SPACE);
         } catch (Exception e){
             e.printStackTrace();
+        } finally {
+            Thread.sleep(4000);
+            driver.quit();
         }
-//        Alert alert = driver.switchTo().alert();
-//        Thread.sleep(5000);
-//        alert.accept();
     }
 
 }
